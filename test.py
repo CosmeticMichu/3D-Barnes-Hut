ae20a59ce187f78c5484f066b08e13827f9cc035
eariasp <eariasp@unal.edu.co>
2023-12-03 11:13:54 -0500

    testing generate_conditions
    function to generate sample of particles with initial conditions is properly working. There is an implementation of it in test.py

--- test.py
+++ test.py
@@ -1,36 +1,39 @@
 from Geometry import *
 from Tree import *
 from Physics import Particle
+import Condiciones as cond
 import matplotlib.pyplot as plt
 
 corner = Vector(0.,0.,0.)
 
 # particles
-p1 = Particle (10, [.4,.3,.6], [0,0,0], [0,0,0])
-p2 = Particle (10, [.55,.7,.7], [0,0,0], [0,0,0])
-p3 = Particle (10, [.6,.8,.6], [0,0,0], [0,0,0])
+# p1 = Particle (10, [4,3,6], [0,0,0], [0,0,0])
+# p2 = Particle (10, [5.5,7,7], [0,0,0], [0,0,0])
+# p3 = Particle (10, [6,8,6], [0,0,0], [0,0,0])
 
-particles = [p1,p2,p3]
+# particles = [p1,p2,p3]
+
+particles = cond.generate_conditions(10,10)
 
 # build tree
-root = Cuboid(corner, 1., 1., 1.)
+root = Cuboid(corner, 10., 10., 10.)
 
-QuadTree = TreeNode(root)
+QuadTree = TreeNode(root, capacity = 1)
 
 #add particles
 for particle in particles:
     QuadTree.insert(particle)
 
 # plot
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-r = [-1,1]
-X, Y = np.meshgrid(r, r)
+ax.set_xlim3d(xmin = 0, xmax = 10)
+ax.set_ylim3d(ymin = 0, ymax = 10)
+ax.set_zlim3d(zmin = 0, zmax = 10)
 
-# ax.set_title(f'El profe lo chupa', fontsize = 18, fontweight = 'bold')
+# ax.set_title(f'title', fontsize = 18, fontweight = 'bold')
 
-QuadTree.draw(ax)
+QuadTree.draw(ax, show_axis = False)
 ax.scatter([particle.x for particle in particles],[particle.y for particle in particles], [particle.z for particle in particles], s=10, c = 'w')
 plt.show()
