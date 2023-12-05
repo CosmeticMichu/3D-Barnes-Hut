bfbf533f55213a7c99af1679e6494dca20ad15ff
eariasp <eariasp@unal.edu.co>
2023-12-05 01:48:30 -0500

    correctly ploting the tree

--- test.py
+++ test.py
@@ -14,17 +14,17 @@ corner = Vector(0.,0.,0.)
 
 # particles = [p1,p2,p3]
 
-# particles = cond.generate_conditions(10,10)
-particles = MakeParticles(10, [10.,10.,10.])
+particles = cond.generate_conditions(10,10, dim = 5., seed = 23)
+# particles = MakeParticles(10, [10.,10.,10.])
 
 # build tree
 root = Cuboid(corner, 10., 10., 10.)
 
-QuadTree = TreeNode(root, capacity = 1)
+Octree = TreeNode(root, capacity = 1)
 
 #add particles
 for particle in particles:
-    QuadTree.insert(particle)
+    Octree.insert(particle)
 
 # plot
 fig = plt.figure()
@@ -36,6 +36,6 @@ ax.set_zlim3d(zmin = 0, zmax = 10)
 
 # ax.set_title(f'title', fontsize = 18, fontweight = 'bold')
 
-QuadTree.draw(ax, show_axis = False)
+OcTree.draw(ax, show_axis = False)
 ax.scatter([particle.x for particle in particles],[particle.y for particle in particles], [particle.z for particle in particles], s=10, c = 'w')
 plt.show()
